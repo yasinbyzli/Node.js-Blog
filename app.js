eb6ecb7872d98be8b0a90357bae937a50178079c
Yasin Beyazli <yasinbeyazli29@gmail.com>
2021-04-12 16:36:06 +0300

    Proje Başlangıç

--- app.js
+++ app.js
@@ -6,25 +6,19 @@ const app = express();
 const hostName = '127.0.0.1';
 const port = 3000;
 
-// ? test url'ne gittiğin zaman console bir şey yazdır.
-// ? middleware 3 adet parametre alır
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    // ? __dirname -> app.js'in bulunduğu dizini verir.
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './site/index.html'));
 })
 
-app.get('/about', (req, res) => {
-    // ? __dirname -> app.js'in bulunduğu dizini verir.
-    res.sendFile(path.resolve(__dirname, 'about.html'));
+app.get('/about', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './site/about.html'));
 })
 
-app.get('/contact', (req, res) => {
-    // ? __dirname -> app.js'in bulunduğu dizini verir.
-    res.sendFile(path.resolve(__dirname, 'contact.html'));
+app.get('/blog', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './site/blog.html'));
 })
-
 
 
 app.listen(port, hostName, () => {
