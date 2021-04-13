48b25e0235e88b0682566edf3438fd8fe4392274
Yasin Beyazli <yasinbeyazli29@gmail.com>
2021-04-13 13:19:02 +0300

    CRUD islemleri

--- test.js
+++ test.js
@@ -9,9 +9,38 @@ mongoose.connect('mongodb://127.0.0.1/nodeblog_test_db', {
   useCreateIndex: true
 });
 
-Post.create({
+// * Tüm verileri getirir
+/* Post.find({}, (error, post) => {
+    console.log(error, post);
+})
+ */
+
+
+// * Postlar içinde arama 
+/* Post.find({
+    title : 'Benim ilk post başlığım'
+}, (error, post) => {
+    console.log(error, post);
+})
+ */
+
+// * Post oluşturma
+/* Post.create({
     title : 'Benim ilk post başlığım',
     content : 'Benim ilk post içeriğim',
 }, (error, post) => {
     console.log(error, post);
+}) */
+
+// * Güncelleme yapma
+// Post.findByIdAndUpdate('60756904993e6a18a4abaaa0', {
+//     title : 'Benim 1. post başlığım'
+// }, (error, post) => {
+//     console.log(error, post);
+// })
+
+
+// * Silme
+Post.findByIdAndDelete('60756904993e6a18a4abaaa0', (error, post) => {
+    console.log(error,post);
 })
