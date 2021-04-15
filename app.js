a8e73d2495a08cb4567a265a2fe4e4734912915d
Yasin Beyazli <yasinbeyazli29@gmail.com>
2021-04-15 13:45:45 +0300

    Linkler düzenlendi

--- app.js
+++ app.js
@@ -1,68 +1,84 @@
-const express = require('express');
+const express = require("express");
 // Template Engine
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 // veritabanı bağlantısı için
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // dosya eklemek için
-const fileUpload = require('express-fileupload'); 
-const generateDate = require('./helpers/generateDate').generateDate;
+const fileUpload = require("express-fileupload");
+const generateDate = require("./helpers/generateDate").generateDate;
 // session
-const expressSession = require('express-session');
-const connectMongo = require('connect-mongo');
+const expressSession = require("express-session");
+const connectMongo = require("connect-mongo");
 
-const hostName = '127.0.0.1';
+const hostName = "127.0.0.1";
 const port = 3000;
 
 // Eğer boyle bir veritabanı yoksa bizim adımıza oluşturur.
-mongoose.connect('mongodb://127.0.0.1/nodeblog_db', {
+mongoose.connect("mongodb://127.0.0.1/nodeblog_db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-  useCreateIndex: true // unique 
+  useCreateIndex: true, // unique
 });
 
 const app = express();
 
 // session işlemi
-app.use(expressSession({
-  secret : 'testotesto', // güvenlik anahtarı
-  resave : false,
-  saveUninitialized : true,
-  store : connectMongo.create({ mongoUrl : 'mongodb://127.0.0.1/nodeblog_db' })
-}))
+app.use(
+  expressSession({
+    secret: "testotesto", // güvenlik anahtarı
+    resave: false,
+    saveUninitialized: true,
+    store: connectMongo.create({ mongoUrl: "mongodb://127.0.0.1/nodeblog_db" }),
+  })
+);
 
 // ? Flash - Message Middleware
 app.use((req, res, next) => {
   res.locals.sessionFlash = req.session.sessionFlash;
   delete req.session.sessionFlash;
   next();
-})
-
+});
 
 // ? Dosya yukleme işlemi için
 app.use(fileUpload());
 
 // ? Middleware
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.engine('handlebars', exphbs({helpers : {generateDate}}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ helpers: { generateDate } }));
+app.set("view engine", "handlebars");
 
 // parse application/x-www-form-urlencoded - Veritabanı verileri yakalayıp kaydetmek için
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 // parse application/json
-app.use(express.json())
+app.use(express.json());
 
-// ? Routes işlemleri - Middleware 
-const main = require('./routes/main');
-const posts = require('./routes/posts');
-const users = require('./routes/users');
+// ? Display link middleware
+app.use((req, res, next) => {
+  const { userId } = req.session;
+  if (userId) {
+    res.locals = {
+      displayLink: true,
+    };
+  } else {
+    res.locals = {
+      displayLink: false,
+    };
+  }
+  next();
+});
 
-app.use('/', main);
-app.use('/posts', posts);
-app.use('/users', users);
+// ? Routes işlemleri - Middleware
+const main = require("./routes/main");
+const posts = require("./routes/posts");
+const users = require("./routes/users");
+
+app.use("/", main);
+app.use("/posts", posts);
+app.use("/users", users);
 
 // ? Server dinleme
 app.listen(port, hostName, () => {
-    console.log(`Server Çalışıyor, http://${hostName}:${port}`);
-})
+  console.log(`Server Çalışıyor, http://${hostName}:${port}`);
+});
--- routes/users.js
+++ routes/users.js
@@ -38,4 +38,10 @@ router.post('/login', (req,res) => {
 
 })
 
+router.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    })
+})
+
 module.exports = router;
