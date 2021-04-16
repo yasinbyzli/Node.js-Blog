e48afde517d9d3f80ff10b529f15c3d3f2dd7135
Yasin Beyazli <yasinbeyazli29@gmail.com>
2021-04-16 12:22:44 +0300

    Kategori ekleme işlemi yapıldı

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+const Category = require('../models/Category');
 
 router.get("/", (req, res) => {
     res.render('/admin/admin')
 })
 
 router.get('/categories', (req, res) => {
-    res.render('admin/categories');
+    Category.find({}).lean().then(categories => {
+        res.render('admin/categories', {categories : categories})
+    })
+})
+
+router.post('/categories', (req, res) => {
+    Category.create(req.body, (error, category) => {
+       if (!error) {
+        res.redirect('/admin/categories')
+       } 
+    });
 })
 
 module.exports = router;
